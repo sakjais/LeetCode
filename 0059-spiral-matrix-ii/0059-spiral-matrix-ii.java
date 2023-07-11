d5be5fea45889f4c8114470d5562ed9a710c3cca
sakjais <100084160+sakjais@users.noreply.github.com>
2023-07-11 10:51:53 +0530

    Time: 0 ms (100.00%), Space: 40.2 MB (97.01%) - LeetHub

--- 0059-spiral-matrix-ii/0059-spiral-matrix-ii.java
+++ 0059-spiral-matrix-ii/0059-spiral-matrix-ii.java
@@ -2,7 +2,7 @@ class Solution {
     public int[][] generateMatrix(int n) {
         int [][] a = new int[n][n];
         int startRow=0,startCol=0,endRow=n-1,endCol=n-1,v=1;
-        while(startCol<=endRow||startRow<=endCol){
+        while(startRow<=endRow||startCol<=endCol){
          if(startCol<=endCol){
              for(int i=startRow;i<=endRow;i++)
                 a[startCol][i]=v++;
