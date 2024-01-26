df4ad568a63fbc71718c1b2cd49fd68dcbff8c80
sakjais <100084160+sakjais@users.noreply.github.com>
2024-01-26 22:36:54 +0530

    Time: 0 ms (100.00%), Space: 42.3 MB (13.05%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,29 +1,22 @@
 class Solution {
     public void rotate(int[][] matrix) {
-        swapRows(matrix);
-        transpose(matrix);
         
-    }
-    public static void swapRows(int [] [] matrix){
-        int n=matrix.length;
-        int start=0;
-        int end=n-1;
-        while(start<end){
-            int [] temp=matrix[start];
-            matrix[start]=matrix[end];
-            matrix[end]=temp;
-            start++;
-            end--;
-        }
-    }
-    public static void transpose(int [][] matrix){
-        int n=matrix.length;
-        for(int i=0;i<n-1;i++){
-            for(int j=i+1;j<n;j++){
-                int temp=matrix[i][j];
-                matrix[i][j]=matrix[j][i];
-                matrix[j][i]=temp;
+        for(int i = 0; i < matrix.length; i++){
+            for(int j = i; j < matrix.length; j++){
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
             }
         }
+        for(int i = 0; i < matrix.length ; i++){
+            int l = 0;
+            int r = matrix.length-1;
+            for(;l<r;l++,r--){
+                int temp = matrix[i][l];
+                matrix[i][l] = matrix[i][r];
+                matrix[i][r] = temp;
+            }
+        }
+        return;
     }
 }
